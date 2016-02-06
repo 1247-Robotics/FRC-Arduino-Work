19870ea265eebbf7aa2f7c7f0ea7400b968d1ad8
1247 Robotics <1247Robtics@gmail.com>
2016-02-06 12:29:27 -0500

    arduino works

--- Driver_Controller/Driver_Controller.ino
+++ Driver_Controller/Driver_Controller.ino
@@ -1,21 +1,56 @@
-const int button = 2;
+#include <LiquidCrystal.h>
 
-int buttonState = 0;
+#define TANK_DRIVE 0
+#define ARCADE_DRIVE 1
+
+#define JOYSTICK 0
+#define XBOX 1
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+const int driveModeButton = 2;
+const int controllerButton = 3;
+
+int driveMode = TANK_DRIVE;
+int controller = JOYSTICK;
 
 void setup() {
   // put your setup code here, to run once:
   Joystick.begin();
-  pinMode(button, INPUT);
+  
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print(" Team 1247 GO!! ");
+  
+  pinMode(driveModeButton, INPUT);
+  pinMode(controllerButton, INPUT);
+  delay(1000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  buttonState = digitalRead(button);
+  driveMode = digitalRead(driveModeButton);
+  controller = digitalRead(controllerButton);
   
-  if (buttonState == HIGH){
+  if (driveMode == ARCADE_DRIVE){
     Joystick.setButton(0,1);
+    lcd.setCursor(0, 0);
+    lcd.print("  Arcade Drive  ");
   } else{
     Joystick.setButton(0,0);
+    lcd.setCursor(0, 0);
+    lcd.print("   Tank Drive   ");
+  }
+  
+  if (controller == XBOX){
+    Joystick.setButton(1,1);
+    lcd.setCursor(0, 1);
+    lcd.print(" XBOX Controller");
+  } else{
+    Joystick.setButton(1,0);
+    lcd.setCursor(0, 1);
+    lcd.print("    Joystick    ");
   }
   delay(10);
 }
