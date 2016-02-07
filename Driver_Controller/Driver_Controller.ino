3146c4748591b16026112d7ab4993f73c7065c7e
1247 Robotics <1247Robtics@gmail.com>
2016-02-07 14:25:13 -0500

    ha

--- Driver_Controller/Driver_Controller.ino
+++ Driver_Controller/Driver_Controller.ino
@@ -6,6 +6,8 @@
 #define JOYSTICK 0
 #define XBOX 1
 
+#define FAKE_GROUND 7
+
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
@@ -25,6 +27,9 @@ void setup() {
   
   pinMode(driveModeButton, INPUT);
   pinMode(controllerButton, INPUT);
+  
+  pinMode(FAKE_GROUND, OUTPUT);
+  
   delay(1000);
 }
 
@@ -32,6 +37,8 @@ void loop() {
   // put your main code here, to run repeatedly:
   driveMode = digitalRead(driveModeButton);
   controller = digitalRead(controllerButton);
+  
+  digitalWrite(FAKE_GROUND, LOW);
   
   if (driveMode == ARCADE_DRIVE){
     Joystick.setButton(0,1);
